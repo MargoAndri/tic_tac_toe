bf9b0d6e68c6cfe97719bd4cb89bf1c0902de504
MargoAndri <margoandriasyan@gmail.com>
2019-06-02 23:03:17 +0300

    Добавлена функции reset и функция определения победителя

--- game.js
+++ game.js
@@ -1,40 +1,91 @@
+import createElement from './create-element.js';
 
 const gameFieldSection = document.querySelector(`.container`);
+const resetGame = document.querySelector(`.reset-game`);
+const message = document.querySelector(`.message`);
+const winCombinations = [
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const playerScore = {
+    X : [],
+    O: []
+};
+let player = `X`;
+
+
 const getTemplate = () => {
     return `
     <div class="field">
-        <div class="item cell-1"></div>
-        <div class="item cell-2"></div>
-        <div class="item cell-3"></div>
-        <div class="item cell-4"></div>
-        <div class="item cell-5"></div>
-        <div class="item cell-6"></div>
-        <div class="item cell-7"></div>
-        <div class="item cell-8"></div>
-        <div class="item cell-9"></div>
+        <div class="item" data-cell="1"></div>
+        <div class="item" data-cell="2"></div>
+        <div class="item" data-cell="3"></div>
+        <div class="item" data-cell="4"></div>
+        <div class="item" data-cell="5"></div>
+        <div class="item" data-cell="6"></div>
+        <div class="item" data-cell="7"></div>
+        <div class="item" data-cell="8"></div>
+        <div class="item" data-cell="9"></div>
     </div>`.trim();
 };
 
-const createElement = (template) => {
-    const newElement = document.createElement(`div`);
-    newElement.innerHTML = template;
-    return newElement.firstChild;
-};
-
-let player = `X`;
 
 const renderGameField = (section) => {
     let newField = createElement(getTemplate());
-    newField.addEventListener('click', (evt) => {
+    newField.addEventListener(`click`, (evt) => {
         evt.preventDefault();
         let cell = evt.target;
         if (cell.textContent === ``) {
+            let cellNumber = cell.getAttribute(`data-cell`);
             cell.textContent = player;
+            playerScore[player].push(+cellNumber);
+            if (playerScore[player].length >= 3 && checkWin()) {
+                message.textContent = `Выиграл игрок ${player}!`;
+            }
             player = (player === `X`) ?`O` : `X`;
         }
     });
 
-    section.appendChild(newField);
+    section.insertBefore(newField, resetGame);
 };
+
+const checkWin = () => {
+    for (let combination of winCombinations) {
+        let count = 0;
+        for(let i of combination) {
+            if (playerScore[player].indexOf(i) === -1) {
+                break;
+            } else {
+                count++;
+                if (count === 3) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+};
+
+const clearField = () => {
+    message.textContent = `Первым ходит игрок Х`;
+    const fieldItems = document.querySelectorAll(`.item`);
+    player = `X`;
+    playerScore.X = [];
+    playerScore.O = [];
+    fieldItems.forEach((item) => {
+        item.textContent = ``;
+    })
+};
+
+resetGame.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    clearField();
+});
 
 renderGameField(gameFieldSection);
